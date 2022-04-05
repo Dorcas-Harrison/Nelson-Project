034f788e4dbb1037c4755ea8bb466445827f9a98
Dorcas-Harrison <71101222+Dorcas-Harrison@users.noreply.github.com>
2022-04-05 07:01:40 -0500

    Update Nelson actual draft.R

--- Nelson actual draft.R
+++ Nelson actual draft.R
@@ -7,13 +7,14 @@ library(zoo)
 library(xts)
 
 
-maturity.oct <- c(2.0,3.0,5.0,7.0,10.0,30.0)
+maturity.oct<- c(2.0,3.0,5.0,7.0,10.0,30.0)
 #average yield rate for October 
 rate.oct <- c(1.551364,1.528182,1.525,1.615,1.706818,2.190455)
 NSparameters <- Nelson.Siegel(rate = rate.oct, maturity = maturity.oct)
 
-y = NSrates(NSparameters, maturity.oct)
+nspar <- xts(NSparameters, Sys.Date())
+
+y = NSrates(nspar, maturity.oct)
 plot(maturity.oct, rate.oct, xlim=c(0,30), ylim=c(1.5,2.2), xlab = "Maturity Years", ylab ="Average Rate", main = "Fitting Nelson-Siegel-Svensson Yield Curve", type = "o")
 lines(maturity.oct,y, col =2)
 legend(x ="topleft", legend = c("observed yield curve", "fitted yield curve"), col = c(1,2), lty=1)
-
